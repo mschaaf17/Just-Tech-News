527e1f5d5a13d1d933d806a15f04eeb8518b2b94
Maddy <madisonschaaf48@gmail.com>
2022-04-09 19:07:23 -0600

    fixed files with sequalize error

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
-const userRoutes = require('./user-routes.js');
-router.use('./users', userRoutes)
+
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+
 module.exports = router;
