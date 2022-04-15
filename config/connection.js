791ad5578cd893b4967ee41ba3345f427b26ad8b
Maddy <madisonschaaf48@gmail.com>
2022-04-14 18:39:22 -0600

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,16 @@ require('dotenv').config();
 
 // create connection to our db
 //need do add npm install dotenv in order to use this
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//need the jaws if we want to deploy to heroku
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+  sequelize= new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
 });
-
+}
 module.exports = sequelize;
