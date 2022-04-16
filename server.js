6e01e603561d2332907b976abadee8fddaa0a975
Maddy <madisonschaaf48@gmail.com>
2022-04-16 17:25:21 -0600

    added logout

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ const path = require('path');
 const express = require('express');
 //set up handlebars (npm install express-handlebars)
 const exphbs = require('express-handlebars');
+const session = require('express-session')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 //set up handlebars (npm install express-handlebars)
 const hbs = exphbs.create({});
